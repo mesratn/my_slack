7d15bcdd3846dd2f56370485525555f5916cce3a
Kumatetsu <billaudjean@gmail.com>
2017-04-21 22:48:05 +0200

    malloc env et modification create user

--- server_nada/main.c
+++ server_nada/main.c
@@ -5,23 +5,21 @@
 ** Login   <mesrat_n@etna-alternance.net>
 ** 
 ** Started on  Fri Apr 21 13:29:26 2017 MESRATI Nada
-** Last update Fri Apr 21 13:33:22 2017 MESRATI Nada
+** Last update Fri Apr 21 21:43:11 2017 BILLAUD Jean
 */
 
 #include "server.h"
 
 int	main(int argc, char **argv)
 {
-  t_env	e;
+  t_env	*e;
   if (argc != 2 || my_getnbr(argv[1]) < 1025)
     {
       my_putstr("Usage : ./server_bin [port > 1025]\n\n");
       return (0);
     }
-  e.port = my_getnbr(argv[1]);
-  e.list = NULL;
-  e.chan = NULL;
-  add_client(&e);
-  while (my_server(&e));
+  e = create_env(my_getnbr(argv[1]));
+  add_client(e);
+  while (my_server(e));
   return (0);
 }
