bef95506ba237b4ad4c617802c5f71c64604f01d
Kumatetsu <billaudjean@gmail.com>
2017-04-22 20:40:50 +0200

    free func

---
+++ server/functions/free_chain.c
@@ -0,0 +1,51 @@
+/*
+** free_chain.c for my_slack in /Volumes/Neku/Projets-Etna/my_slack
+** 
+** Made by BILLAUD Jean
+** Login   <billau_j@etna-alternance.net>
+** 
+** Started on  Sat Apr 22 20:18:58 2017 BILLAUD Jean
+** Last update Sat Apr 22 20:40:03 2017 BILLAUD Jean
+*/
+
+#include "../headers/server.h"
+
+void		free_all_users(t_env *e)
+{
+  t_user	*u;
+
+  u = e->list;
+  while (u)
+    {
+      e->list = u->next;
+      free_user(u);
+      u = e->list;
+    }
+}
+
+void		free_all_chan(t_env *e)
+{
+  t_chan	*c;
+  
+  c = e->chan;
+  while (c)
+    {
+      free_all_node(c);
+      e->chan = c->next;
+      free_chan(c);
+      c = e->chan;
+    }
+}
+
+void		free_all_node(t_chan *c)
+{
+  t_node	*n;
+
+  n = c->first;
+  while (n)
+    {
+      c->first = n->next;
+      free_node(n);
+      n = c->first;
+    }
+}
--- server/functions/free_struct.c
+++ server/functions/free_struct.c
@@ -5,6 +5,31 @@
 ** Login   <billau_j@etna-alternance.net>
 ** 
 ** Started on  Sat Apr 22 19:49:52 2017 BILLAUD Jean
-** Last update Sat Apr 22 19:51:59 2017 BILLAUD Jean
+** Last update Sat Apr 22 20:19:24 2017 BILLAUD Jean
 */
 
+#include "../headers/server.h"
+
+void	free_user(t_user *u)
+{
+  if (u->login != NULL)
+    free(u->login);
+  close(u->fd);
+  free(u);
+}
+
+void	free_node(t_node *n)
+{
+  free(node);
+}
+
+void	free_chan(t_chan *c)
+{
+  free(c->name);
+  free(c);
+}
+
+void	free_env(t_env *env)
+{
+  free(e);
+}
